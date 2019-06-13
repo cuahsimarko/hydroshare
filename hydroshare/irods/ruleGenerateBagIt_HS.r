c2e75f65e774a119f835cb55899f864c8b81eda0
Alva Couch <acouch@cuahsi.org>
2019-06-13 13:40:38 -0400

    Comments on how to replace BagitRule

--- hydroshare/irods/ruleGenerateBagIt_HS.r
+++ hydroshare/irods/ruleGenerateBagIt_HS.r
@@ -19,11 +19,25 @@ generateBagIt {
 ### - with bagit specification since the default SHA256 hash
 ### - scheme is base64 encoded, which does not meet hex-encoded
 ### - requirement for checksums in bagit specification.
+
+#########################
+WHAT THIS SCRIPT DOES
+#########################
+### BAGITDATA is the root of a resource. 
+### BAGITDATA/data/contents are the resource files. 
+### BAGITDATA is of the prefix-path/{32-digit resource id}
+### At start, BAGITDATA contains resource data but not metadata. 
+### We write metadata: 
 ### - writes bagit.txt to BAGITDATA/bagit.txt
 ### - generates payload manifest file of BAGITDATA/data
 ### - writes payload manifest to BAGITDATA/manifest-md5.txt
 ### - writes tagmanifest file to BAGITDATA/tagmanifest-md5.txt
 ### - writes to rodsLog
+### Final step is to zip the bag, into the output directory
+
+Two resources: bagit specification: https://en.wikipedia.org/wiki/BagIt
+Rule language: https://docs.irods.org/4.1.5/manual/rule_language/
+
 #
 # -----------------------------------------------------
 
@@ -40,21 +54,26 @@ generateBagIt {
   *OFFSET = int(*ROOTLENGTH) + 1;
   *NEWBAGITDATA = "*BAGITDATA" ++ "/data";
   *ContInxOld = 1;
+  ############################
+  ### Ask Irods where the files are. 
+  ### A genQuery is an irods LS command
+  ############################
+
   *Condition = "COLL_NAME like '*NEWBAGITDATA%%'";
   msiMakeGenQuery("DATA_ID, DATA_NAME, COLL_NAME, DATA_CHECKSUM", *Condition, *GenQInp);
   msiExecGenQuery(*GenQInp, *GenQOut);
   msiGetContInxFromGenQueryOut(*GenQOut, *ContInxNew);
   while(*ContInxOld > 0) {
     foreach(*GenQOut) {
-      msiGetValByKey(*GenQOut, "DATA_NAME", *Object);
-      msiGetValByKey(*GenQOut, "COLL_NAME", *Coll);
-      msiGetValByKey(*GenQOut, "DATA_CHECKSUM", *CHKSUM);
+      msiGetValByKey(*GenQOut, "DATA_NAME", *Object); # name of the file to bag 
+      msiGetValByKey(*GenQOut, "COLL_NAME", *Coll);  # containing directory
+      msiGetValByKey(*GenQOut, "DATA_CHECKSUM", *CHKSUM); # checksum
       ### - only recalculate checksum when the checksum retrieved from iCAT
       ### - is empty or not MD5, but sha256 prefixed which is the default hash scheme
       ### - to accommodate the case that the checksum stored in iCAT might
       ### - be generated before default hash scheme is changed to MD5
-      *FULLPATH = "*Coll" ++ "/" ++ "*Object";
-      if (strlen(*CHKSUM) > 5) {
+      *FULLPATH = "*Coll" ++ "/" ++ "*Object"; # full pathname of file to bag 
+      if (strlen(*CHKSUM) > 5) { # skip this. Not relevant
         *PrefixStr = substr(*CHKSUM, 0, 5);
         if (*PrefixStr == 'sha2:') {
           msiDataObjChksum(*FULLPATH, "forceChksum=", *CHKSUM);
@@ -64,8 +83,8 @@ generateBagIt {
         msiDataObjChksum(*FULLPATH, "forceChksum=", *CHKSUM);
       }
 
-      msiSubstr(*FULLPATH, str(*OFFSET), "null", *RELATIVEPATH);
-      writeString("stdout", *CHKSUM);
+      msiSubstr(*FULLPATH, str(*OFFSET), "null", *RELATIVEPATH); # creates a relative path 
+      writeString("stdout", *CHKSUM); 
       writeLine("stdout", "    *RELATIVEPATH")
     }
     *ContInxOld = *ContInxNew;
@@ -75,6 +94,11 @@ generateBagIt {
   }
 
   ### - writes payload manifest to BAGITDATA/manifest-md5.txt
+
+  ############################
+  ### open a file manifest-md5.txt
+  ############################
+  ###              FILENAME                 relative to RESOURCE NAME 
   msiDataObjCreate("*BAGITDATA" ++ "/manifest-md5.txt", "destRescName=" ++ "*DESTRESC" ++ "++++forceFlag=", *FD);
   msiDataObjWrite(*FD, "stdout", *WLEN);
   msiDataObjClose(*FD, *Status);
@@ -91,6 +115,10 @@ generateBagIt {
   msiDataObjChksum("*BAGITDATA" ++ "/readme.txt", "forceChksum=", *CHKSUM);
   writeString("stdout", *CHKSUM);
   writeLine("stdout", "    readme.txt")
+
+  ############################
+  ### open a file tagmanifest-md5.txt
+  ############################
   msiDataObjCreate("*BAGITDATA" ++ "/tagmanifest-md5.txt", "destRescName=" ++ "*DESTRESC" ++ "++++forceFlag=", *FD);
   msiDataObjWrite(*FD, "stdout", *WLEN);
   msiDataObjClose(*FD, *Status);
